f9d8a56da01c53cc847925b638c321f42e7c4209
Vikas-841 <150503522+Vikas-841@users.noreply.github.com>
2023-11-27 23:41:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919156737478', 'Vikas', true],
   [''] 
 ] //Number of owners
 
